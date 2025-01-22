595939cd7f89c6405d6a816cd75843ed9c47546f
Renu-jain109 <jainwithrenu243@gmail.com>
2025-01-22 23:49:56 +0530

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideToastr } from 'ngx-toastr';
-
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { AuthinterceptorService } from './services/authinterceptor.service';
 
 
 export const appConfig: ApplicationConfig = {
@@ -18,6 +17,11 @@ export const appConfig: ApplicationConfig = {
         theme: {
             preset: Aura
         }
-    }),provideHttpClient(),importProvidersFrom([BrowserAnimationsModule]),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+    }),provideHttpClient(withInterceptorsFromDi()),importProvidersFrom([BrowserAnimationsModule]),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
+    ,{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthinterceptorService,
+        multi: true,
+      },]
 };
 
